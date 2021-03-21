01b118386240baa26e67260b3a055899bb7a36f2
walsc024 <chriswalsh08@googlemail.com>
2021-03-21 16:02:42 +0000

    final changes

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,22 +1,72 @@
-// Declare our cache variable names
-var CACHE = "budget-tracker";
-var DATA_CACHE = "budget-tracker-responses";
-
-//Map out which URLs we are going to cache
-var applicationUrls = [
+const FILES_TO_CACHE = [
+    "/icons/icon-192x192.png",
+    "/icons/icon-512x512.png",,
+    "/icons/budget.png",
     "/",
-    "/db.js",
+    "/index.html",
     "/index.js",
-    "/manifest.json",
+    "/manifest.webmanifest",
+    "/db.js",
     "/styles.css",
-    "/icons/inon-192x192.png",
-    "/icons/icon-512x512.png"
-]
-
-self.addEventListener('install', function(event) {
-    event.waitUntil(
-        caches.open(CACHE).then((cache) =>{
-            return cache.addAll(applicationUrls)
-        })
-    )
-})
+  ];
+  
+  const CACHE_NAME = "static-cache-v2";
+  const DATA_CACHE_NAME = "data-cache-v1";
+  // code to install and register your service worker
+  self.addEventListener("install", function (evt) {
+    evt.waitUntil(
+      caches.open(CACHE_NAME).then((cache) => {
+        console.log("Your files were pre-cached successfully!");
+        return cache.addAll(FILES_TO_CACHE);
+      })
+    );
+    self.skipWaiting();
+  });
+  //code to activate the service worker and remove old data from the cache
+  self.addEventListener("activate", function (evt) {
+    evt.waitUntil(
+      caches.keys().then((keyList) => {
+        return Promise.all(
+          keyList.map((key) => {
+            if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+              console.log("Removing old cache data", key);
+              return caches.delete(key);
+            }
+          })
+        );
+      })
+    );
+    self.clients.claim();
+  });
+  //Enable the service worker to intercept network requests
+  self.addEventListener("fetch", function (evt) {
+    if (evt.request.url.includes("/api/")) {
+      evt.respondWith(
+        caches.open(DATA_CACHE_NAME).then((cache) => {
+            return fetch(evt.request)
+              .then((response) => {
+                // If the response was good, clone it and store it in the cache.
+                if (response.status === 200) {
+                  cache.put(evt.request.url, response.clone());
+                }
+                return response;
+              })
+              .catch((err) => {
+                // Network request failed, try to get it from the cache.
+                return cache.match(evt.request);
+              });
+          })
+          .catch((err) => console.log(err))
+      );
+  
+      return;
+    }
+    // code to handle requests goes here
+    evt.respondWith(
+      caches.open(CACHE_NAME).then((cache) => {
+        return cache.match(evt.request).then(response => {
+          return response || fetch(evt.request);
+        });
+      })
+    );
+  });
